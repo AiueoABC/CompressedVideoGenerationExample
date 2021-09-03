60416e7aa7e6401bbb73de1b9216186908492165
AiueoABC <jtktchinou@gmail.com>
2021-09-03 22:10:31 +0900

    Add Feature: Made it available to use h264_nvenc(GPU acceleration)

--- CompressedVideoGenerationExample.py
+++ CompressedVideoGenerationExample.py
@@ -1,35 +1,65 @@
 import cv2
+import numpy as np
 
 
 class Frames2Movie:
-    def __init__(self):
+    def __init__(self, use_nvenc=False):
         self.frames = []  # [(epochTime0, frame0), (epochTime1, frame1), ...]
         self.fps = 10.  # target fps, this example handles a constant framerate video.
         self.codec = 'h264'  # Using h264, to use more efficient codecs, like av1, pls compile opencv from its source.
         self.filename = './video.mp4'
         self.timecodes_filename = './timecodes.txt'
+        self._use_nvenc = use_nvenc
+        if self._use_nvenc:
+            try:
+                import ffmpeg
+                self._ffmpeg = ffmpeg
+            except Exception:
+                print('Install ffmpeg-python first => pip install ffmpeg-python')
+                self._use_nvenc = False
 
     def convert2video(self):
         print('Start Encoding')
         height, width = self.frames[0][1].shape[:2]
-        _codec = cv2.VideoWriter_fourcc(*self.codec)
-        _videowriter = cv2.VideoWriter(self.filename, _codec, self.fps, (width, height))
-        _spf = 1 / self.fps
-        _old_frame = ()
-        _skipped_frames = 0
+        _first_timestamp = None
         _time2fill = []
-        for _frame in self.frames:
-            if _old_frame == ():
-                _videowriter.write(_frame[1])
-                _old_frame = _frame
-                _time2fill.append(0.0)
-            else:
-                _time2fill.append(_frame[0] - _old_frame[0])
-                _videowriter.write(_frame[1])
+        if self._use_nvenc:
+            vcodec = f'{self.codec}_nvenc'
+            process = (
+                self._ffmpeg
+                    .input('pipe:', format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(width, height))
+                    .output(self.filename, pix_fmt='yuv420p', vcodec=vcodec, r=self.fps)
+                    .overwrite_output()
+                    .run_async(pipe_stdin=True)
+            )
+            for (_timestamp, _frame) in self.frames:
+                process.stdin.write(
+                    frame
+                        .astype(np.uint8)
+                        .tobytes()
+                )
+                if _first_timestamp is None:
+                    _first_timestamp = _timestamp
+                    _time2fill.append(0.0)
+                else:
+                    _time2fill.append(_timestamp - _first_timestamp)
+            process.stdin.close()
+            process.wait()
+        else:
+            _codec = cv2.VideoWriter_fourcc(*self.codec)
+            _videowriter = cv2.VideoWriter(self.filename, _codec, self.fps, (width, height))
+            for (_timestamp, _frame) in self.frames:
+                _videowriter.write(_frame)
+                if _first_timestamp is None:
+                    _first_timestamp = _timestamp
+                    _time2fill.append(0.0)
+                else:
+                    _time2fill.append(_timestamp - _first_timestamp)
 
-        _videowriter.release()
+            _videowriter.release()
         with open(self.timecodes_filename, 'w') as f:
-            f.writelines(_time2fill)
+            # Store timestamp in millis
+            f.write('\n'.join([str(int(i * 1000)) for i in _time2fill]))
         print(f'''Finish Encoding:
     Processed Frames: {len(self.frames)}
     Recorded FPS: {self.fps}
@@ -40,11 +70,11 @@ if __name__ == '__main__':
     import time  # to get epochTime
     import threading
 
-    each_video_length = 30  # seconds
+    each_video_length = 5  # seconds
     count = 0  # for filename
     target_fps = 60.
 
-    videomaker = Frames2Movie()
+    videomaker = Frames2Movie(use_nvenc=True)
     videomaker.fps = target_fps
 
     cap = cv2.VideoCapture(0)  # Use camera to get frames
